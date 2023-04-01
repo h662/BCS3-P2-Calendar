e0733d3fcd6613942f3420256c70a7b021a516de
h662Olbm <h662@olbm.app>
2023-04-01 21:21:14 +0900

    Date

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,13 @@
+import Date from "./Date";
+
 function Calendar() {
   return (
-    <div className=" bg-white w-1/2 rounded-lg shadow-2xl p-8">Calendar</div>
+    <div className=" bg-white w-1/2 rounded-lg shadow-2xl p-8">
+      <div className="pb-8 text-4xl font-black">2023, 04</div>
+      <ul>
+        <Date />
+      </ul>
+    </div>
   );
 }
 
---
+++ src/components/Date.jsx
@@ -0,0 +1,15 @@
+function Date() {
+  return (
+    <li className="inline-block bg-white rounded-lg shadow-md">
+      <div className=" bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
+        2
+      </div>
+      <ul className="p-2 text-lg">
+        <li>- 청소하기</li>
+        <li>- 빨래하기</li>
+      </ul>
+    </li>
+  );
+}
+
+export default Date;
