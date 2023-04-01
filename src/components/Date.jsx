2e42be6196b7bfb46b5b9aca799a4a8620805c4c
h662Olbm <h662@olbm.app>
2023-04-01 21:34:08 +0900

    calendarData

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,12 +1,21 @@
-function Date() {
+function Date({ date, day, todos }) {
   return (
     <li className="inline-block bg-white rounded-lg shadow-md">
-      <div className=" bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
-        2
+      <div
+        className={`rounded-t-lg px-2 py-1 text-xl text-white ${
+          day === "SAT"
+            ? "bg-orange-400"
+            : day === "SUN"
+            ? "bg-red-400"
+            : "bg-blue-400"
+        }`}
+      >
+        {date}
       </div>
-      <ul className="p-2 text-lg">
-        <li>- 청소하기</li>
-        <li>- 빨래하기</li>
+      <ul className="p-2 text-lg h-20">
+        {todos.map((v, i) => {
+          return <li key={i}>{v}</li>;
+        })}
       </ul>
     </li>
   );
